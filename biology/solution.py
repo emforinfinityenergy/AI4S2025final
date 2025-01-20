789e3f6d573bec2c7259cfda6853fe2d8d5d4daa
EM <energymc@qq.com>
2025-01-20 19:27:19 +0800

    Feat: Code for the biology problem A Score: 0.0135/1 :(

---
+++ biology/solution.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import time
+
+
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, dna, e_score):
+        super().__init__()
+        self.dna = dna
+        self.e_score = e_score
+
+    def __len__(self):
+        return len(self.dna)
+
+    def __getitem__(self, idx):
+        return self.dna[idx], self.e_score[idx]
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(8, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, 16),
+            nn.Tanh(),
+            nn.Linear(16, 1),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+
+t1 = time.time()
+df = pd.read_csv("train.csv")
+dna = []
+e_scores = []
+
+for i in range(df.shape[0]):
+    seg = df['DNA'][i]
+    e_scores.append(df['E-score'][i])
+    idx_l = ['', 'A', 'C', 'G', 'T']
+    seg_l = []
+    for n in seg:
+        seg_l.append(idx_l.index(n))
+    dna.append(seg_l)
+
+dna_t = torch.tensor(dna, dtype=torch.float32)
+e_scores_t = torch.tensor(e_scores, dtype=torch.float32)
+
+dataset = MyDataset(dna_t, e_scores_t)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+
+NUM_EPOCHS = 450
+net = Net()
+criterion = nn.MSELoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+for epoch in range(NUM_EPOCHS):
+    for dna_seg, e_score_seg in dataloader:
+        optimizer.zero_grad()
+        pred = net(dna_seg)
+        loss = criterion(pred.T[0], e_score_seg)
+        loss.backward()
+        optimizer.step()
+    if epoch % 10 == 0:
+        with torch.no_grad():
+            pred = net(dna_t)
+            loss = criterion(pred.T[0], e_scores_t)
+            print(f"Epoch {epoch}, loss = {loss}")
+
+
+# GGGTATCA
+print(net(torch.tensor([3, 3, 3, 4, 1, 4, 2, 1], dtype=torch.float32)))
+# CCGGCCGG
+print(net(torch.tensor([2, 2, 3, 3, 2, 2, 3, 3], dtype=torch.float32)))
+print(time.time() - t1)
